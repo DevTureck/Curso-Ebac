b962c75fd8c921507d29f0f97b150d9ae2d7536c
Miguel Tureck Ramos <migueltureck.ramos@gmail.com>
2024-02-14 22:38:11 -0300

    aulas de compilação de LESS com GRUNT

---
+++ GRUNT/gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done= this.async();
+        setTimeout(function() {
+            console.log('Olá Grunt');
+            done();
+        }, 3000)
+        
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['olaGrunt']);
+}
